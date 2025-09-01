04d5fb2301b28a52699a8583213e2c8eda3baac6
Divya Das <divya.das@trilio.io>
2025-09-01 22:52:27 +0530

    Printing while traversing sll.

--- linkedlists/sll.py
+++ linkedlists/sll.py
@@ -5,33 +5,48 @@ class Node:
         
 
 if __name__ == '__main__':
+    # head = None
+    # n1 = Node(10)
+    # print(n1.data)
+    # print(n1.next)
+    # head = n1
+    
+    # # second Node
+    # n2 = Node(20)
+    # print(n2.data)
+    # print(n2.next)
+    
+    # # Third Node.
+    # n3 = Node(30)
+    # print(n3.data)
+    # print(n3.next)
+    
+    # # Fourth Node
+    # n4 = Node(40)
+    # print(n4.data)
+    # print(n4.next)
+    
+    # # connect nodes.
+    # n1.next = n2
+    # n2.next = n3
+    # n3.next = n4
+    
+    # # Traversing.
+    # while(head != None):
+    #     print(head.data)
+    #     print(id(head.next))
+    #     if(not head.data):
+    #         pass
+    #     head = head.next
+    import random
     head = None
-    n1 = Node(10)
-    print(n1.data)
-    print(n1.next)
-    head = n1
-    
-    # second Node
-    n2 = Node(20)
-    print(n2.data)
-    print(n2.next)
-    
-    # Third Node.
-    n3 = Node(30)
-    print(n3.data)
-    print(n3.next)
-    
-    # Fourth Node
-    n4 = Node(40)
-    print(n4.data)
-    print(n4.next)
-    
-    # connect nodes.
-    n1.next = n2
-    n2.next = n3
-    n3.next = n4
-    
-    while(head != None):
+    count = 0
+    while(not head):
+        head = Node(random.randint(10,50))
         print(head.data)
-        print(id(head.next))
         head = head.next
+        count += 1
+        if(count== 20):
+            break
+        
+        
