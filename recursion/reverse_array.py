343bfaff022f7459f07e1a0038e0dcd9604a74ae
Divya Das <divya.das@trilio.io>
2025-09-08 20:44:51 +0530

    Optimized way of reversing array using recursion.

--- recursion/reverse_array.py
+++ recursion/reverse_array.py
@@ -1,10 +1,21 @@
+import array
+
 def reverse_array(arr, i, n, rev_arr):
     if(i == n):
         return
     
     reverse_array(arr, i+1, n, rev_arr)
-    rev_arr.append(arr[i])
+    rev_arr[i] = arr[i]
     
     return rev_arr
 
-print(reverse_array([10,20,30,40,50], 0, 5, []))
+def optimized_reverse_array(arr, i, j):
+    if(i==j):
+        return
+    
+    arr[i], arr[j] = arr[j], arr[i]
+    optimized_reverse_array(arr, i+1, j-1)
+    return arr
+
+# print(reverse_array([10,20,30,40,50], 0, 5, array.array("i", [])))
+print(optimized_reverse_array([10,20,30,40,50], 0, 4))
