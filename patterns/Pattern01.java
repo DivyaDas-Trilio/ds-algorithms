70f689de80be4d0c86aac4456e2af59c5b3fb3b4
Divya Das <divya.das@trilio.io>
2025-09-26 19:00:39 +0530

    pattern-01

---
+++ patterns/Pattern01.java
@@ -0,0 +1,12 @@
+public class Pattern01 {
+    static int n = 10;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
