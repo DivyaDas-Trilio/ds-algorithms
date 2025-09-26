779da35c1248e8d3eb40b48b446ef20f401c8d1f
Divya Das <divya.das@trilio.io>
2025-09-26 19:18:05 +0530

    pattern_02 in java

---
+++ patterns/pattern_02.java
@@ -0,0 +1,12 @@
+public class pattern_02 {
+    public static int n = 10;
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
